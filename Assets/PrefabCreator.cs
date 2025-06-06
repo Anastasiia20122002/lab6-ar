eac907e8a201134d720598ba9a0eaa6512e38991
kushnirova-anastasiia-fr <kushnirova.anastasiia.fr@gmail.com>
2025-06-06 13:28:41 +0300

    new models update

--- Assets/PrefabCreator.cs
+++ Assets/PrefabCreator.cs
@@ -7,9 +7,11 @@ public class PrefabCreator : MonoBehaviour
 {
     [SerializeField] private GameObject dragonPrefab;
     [SerializeField] private GameObject castlePrefab;
+    [SerializeField] private GameObject thirdPrefab;
 
     [SerializeField] private Vector3 dragonPrefabOffset;
     [SerializeField] private Vector3 castlePrefabOffset;
+    [SerializeField] private Vector3 thirdPrefabOffset;
 
     private ARTrackedImageManager arTrackedImageManager;
     private Dictionary<TrackableId, List<GameObject>> spawnedObjects = new();
@@ -55,33 +57,45 @@ public class PrefabCreator : MonoBehaviour
             spawnedObjects[image.trackableId] = new List<GameObject>();
         }
 
-        // Спавнимо дракончика
+        // Spawn Dragon
         GameObject dragon = Instantiate(dragonPrefab, image.transform.position + dragonPrefabOffset, image.transform.rotation);
         dragon.transform.SetParent(image.transform);
         spawnedObjects[image.trackableId].Add(dragon);
 
-        // Спавнимо знищену вежу
+        // Spawn Castle
         GameObject castle = Instantiate(castlePrefab, image.transform.position + castlePrefabOffset, image.transform.rotation);
         castle.transform.SetParent(image.transform);
         spawnedObjects[image.trackableId].Add(castle);
+
+        // Spawn Third Object
+        GameObject third = Instantiate(thirdPrefab, image.transform.position + thirdPrefabOffset, image.transform.rotation);
+        third.transform.SetParent(image.transform);
+        spawnedObjects[image.trackableId].Add(third);
     }
 
     private void UpdatePrefabsPosition(ARTrackedImage image)
     {
         if (spawnedObjects.TryGetValue(image.trackableId, out List<GameObject> existingObjects))
         {
-            // Dragon Position & Rotation
+            // Update Dragon
             if (existingObjects.Count > 0)
             {
                 existingObjects[0].transform.position = image.transform.position + dragonPrefabOffset;
                 existingObjects[0].transform.rotation = image.transform.rotation;
             }
 
-            // Castle Position & Rotation
+            // Update Castle
             if (existingObjects.Count > 1)
             {
                 existingObjects[1].transform.position = image.transform.position + castlePrefabOffset;
                 existingObjects[1].transform.rotation = image.transform.rotation;
+            }
+
+            // Update Third Object
+            if (existingObjects.Count > 2)
+            {
+                existingObjects[2].transform.position = image.transform.position + thirdPrefabOffset;
+                existingObjects[2].transform.rotation = image.transform.rotation;
             }
         }
     }
